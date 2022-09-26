848f3383e79394820b03bb048cb01107b36210f0
mcgidoug <mcgidoug@gmail.com>
2022-09-26 10:05:34 -0500

    completed madlib function

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,19 @@
 # string concat 
 
-youtuber = ""
+# youtuber = ""
+
 # a few examples
-print("subscribe to " + youtuber)
-print("subscribe to {}".format(youtuber))
-print(f"subscribe to {youtuber}")
+
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all of the time because \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}."
+
+print(madlib)
